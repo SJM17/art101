e653aadd7cffe8e550d1e4f39c213a46275caf86
SJM17 <sophiemorris9896@gmail.com>
2025-04-21 11:13:06 -0700

    Lab 5
    Data Types and variables

---
+++ lab5/js/lab5.js
@@ -0,0 +1,21 @@
+// index.js - Lab 5 - Data Types and Variables
+// Author: Sophie Morris
+// Date: April 21st - 2025
+
+// Define Variables
+make = "Volkswagen";
+model = "Golf GTI";
+color = "White";
+year = 2012;
+
+
+//calculate
+age = 2025 - year;
+
+
+//output
+document.writeln ("Make: " + make + "<br>");
+document.writeln ("Model: " + model + "<br>");
+document.writeln ("Color: " + color + "<br>");
+document.writeln ("Year: " + year + "<br>");
+document.writeln ("Age: " + age + " years<br>");
