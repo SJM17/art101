23ba797a5c349f743487d90d0a1f8a452f236a8d
SJM17 <sophiemorris9896@gmail.com>
2025-05-05 11:07:00 -0700

    Lab 9
    jQuery, CSS, JS, HTML

---
+++ lab9/js/lab.js
@@ -0,0 +1,45 @@
+// lab.js - Lab 9 Libraries and jQuery
+// Requirements: jQuery must be loaded for this script to work
+// Author: Sophie Morris
+// Date: 5/5/25
+
+
+//add button to challenge section
+$("#challenge").append("<button id='button-challenge'>Make Special</button>");
+
+//add a click listener to the challenge button
+$("#button-challenge").click(function(){
+  //now add (or subtract) the "special" class to the section
+  $("#challenge").toggleClass("special");
+});
+
+
+
+//problems button
+$("#problem").append("<button id='button-challenge'>Make Special</button>");
+
+//add a click listener to the challenge button
+$("#button-challenge").click(function(){
+  //now add (or subtract) the "special" class to the section
+  $("#problem").toggleClass("special");
+});
+
+
+//reflection button
+$("#reflection").append("<button id='button-challenge'>Make Special</button>");
+
+//add a click listener to the challenge button
+$("#button-challenge").click(function(){
+  //now add (or subtract) the "special" class to the section
+  $("#reflection").toggleClass("special");
+});
+
+
+//results button
+$("#results").append("<button id='button-challenge'>Make Special</button>");
+
+//add a click listener to the challenge button
+$("#button-challenge").click(function(){
+  //now add (or subtract) the "special" class to the section
+  $("#results").toggleClass("special");
+});
