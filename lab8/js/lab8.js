687ce0b51af56891aebe11542435e20df5fba353
SJM17 <sophiemorris9896@gmail.com>
2025-05-28 19:30:29 -0700

    Lab 14
    Lab 8 Task X Bonus

--- lab8/js/lab8.js
+++ lab8/js/lab8.js
@@ -2,24 +2,37 @@
 // Author: Sophie Morris
 // Date: 5/1/25
 
-function isOdd(x){
+function isOdd(x) {
   return (x % 2 !== 0);
-  }
+}
 
-//test function
-console.log("Is 3 odd? ", isOdd(3));
-console.log("Is 4 odd? ", isOdd(4));
+// Get the output div
+const outputDiv = document.getElementById('output');
 
-array = [34,59,8,102,307,74,3.14,-6,-12,-15]
-console.log("My array ", array);
+// Create output HTML
+let outputHTML = '';
+
+// Test function and add to output
+outputHTML += `<p>Is 3 odd? ${isOdd(3)}</p>`;
+outputHTML += `<p>Is 4 odd? ${isOdd(4)}</p>`;
+
+const array = [34, 59, 8, 102, 307, 74, 3.14, -6, -12, -15];
+outputHTML += `<p>My array: [${array.join(', ')}]</p>`;
 
 let result = array.map(isOdd);
-console.log("Test of oddness of array: ", result);
+outputHTML += `<p>Test of oddness of array: [${result.join(', ')}]</p>`;
 
-let results = array.map(function(x){
+let results = array.map(function(x) {
   return x * 8;
-})
+});
+outputHTML += `<p>Array Numbers Times 8: [${results.join(', ')}]</p>`;
 
+// Set the output div's content
+outputDiv.innerHTML = outputHTML;
+
+// Keep the console logs for debugging if needed
+console.log("Is 3 odd? ", isOdd(3));
+console.log("Is 4 odd? ", isOdd(4));
+console.log("My array ", array);
+console.log("Test of oddness of array: ", result);
 console.log("Array Numbers Times 8: ", results);
-
-
